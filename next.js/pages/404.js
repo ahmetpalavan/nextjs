059bbaaac88e81c7b5b4ba531cbf1dc1871cdddc
ahmetpalavan <ahmetpalavann@gmail.com>
2022-08-12 18:57:07 +0300

    update 404.js

--- next.js/pages/404.js
+++ next.js/pages/404.js
@@ -1,18 +1,18 @@
 import Link from 'next/link'
 import { useEffect } from 'react';
 import {useRouter} from 'next/router'
-
+import ErrorOutlineRoundedIcon from '@mui/icons-material/ErrorOutlineRounded';
 export default function NotFound() {
     const router=useRouter();
     useEffect(()=>{
         setTimeout(()=>{
             // router.push('/')
             router.back()
-        },1000)
+        },100000000000000000)
     },[])
     return (
-        <div className='not-found'>
-            <h1>Oooops...</h1>
+        <div className='not-found' >
+            <h1> <ErrorOutlineRoundedIcon fontSize='large'/></h1>
             <h2>404 |  Sayfa Bulunamadı</h2>
             <p>Lütfen <Link href="/"><a>Anasayfa</a></Link> için tıklayınız</p>
         </div>
